4b0220a0128f7435770818e0f0fe7a779d942abd
blackbeltbase <54255614+blackbeltbase@users.noreply.github.com>
2020-04-13 15:50:10 -0700

    Updated IntroToArrayLists

--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -3,6 +3,7 @@
 package intro_to_array_lists;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class IntroToArrayLists {
 	public static void main(String[] args) {
@@ -14,24 +15,34 @@ public class IntroToArrayLists {
 			list.add("You are "+i);
 		}
 		//3. Print all the Strings using a standard for-loop
+		System.out.println("for");
 		for(int i = 0; i< list.size(); i++) {
 			System.out.println(list.get(i));
 		}
 		//4. Print all the Strings using a for-each loop
+		System.out.println("for each");
 		for(String s:list) {
 			System.out.println(s);
 		}
 		//5. Print only the even numbered elements in the list.
+		System.out.println("even");
 		for(int i = 0; i< list.size(); i++) {
 			if(i%2 == 0) {
 			System.out.println(list.get(i));}
 		}
+		System.out.println("reverse");
 		//6. Print all the Strings in reverse order.
-		for(int i = list.size(); i>0; i--) {
+		for(int i = list.size()-1; i>0; i--) {
 			
 			System.out.println(list.get(i));
 		}
 		//If any words have e:
+		System.out.println("if e");
+		Random ran = new Random();
+		int rando = ran.nextInt(list.size());
+		for(int i = 0; i<rando;i++) {
+			list.add("eeee"+ i);
+		}
 		//7. Print only the Strings that have the letter 'e' in them.
 		for(int i = 0; i< list.size(); i++) {
 			String s = list.get(i);
